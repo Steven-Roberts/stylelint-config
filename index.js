a5b35722173fdb09f2c4417dcb8123537511a446
Steven Roberts <sroberts994@gmail.com>
2018-09-30 16:36:28 -0400

    Add rules from stylelint 9.6.0

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ module.exports = {
         'no-descending-specificity': true,
         'no-duplicate-at-import-rules': true,
         'no-duplicate-selectors': true,
+        'no-empty-first-line': true,
         'no-empty-source': true,
         'no-eol-whitespace': true,
         'no-extra-semicolons': true,
